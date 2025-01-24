26c4d1519cea80e2dc81b755cb559b51f9c6b378
anubhav126 <tinfinite1248@gmail.com>
2025-01-25 00:29:40 +0530

    first-commit

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import Head from 'next/head';
+
+import '../styles/globals.css';
+
+const MyApp = ({ Component, pageProps }) => (
+  <>
+    <Head>
+      <title>Metaversus</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet"></link>
+    </Head>
+    <Component {...pageProps} />
+  </>
+);
+
+export default MyApp;
