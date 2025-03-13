31159f5294a81ad586a17311f07ce47ff9f8bbe4
anubhav126 <tinfinite1248@gmail.com>
2025-03-13 17:46:13 +0530

    final commit

--- pages/_document.js
+++ pages/_document.js
@@ -1,23 +1,20 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
-class MyDocument extends Document {
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link 
-            href="https://stijndv.com/fonts/Eudoxus-Sans.css" 
-            rel="stylesheet" 
-            crossOrigin="anonymous"
-          />
-        </Head>
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <link
+          href="https://stijndv.com/fonts/Eudoxus-Sans.css"
+          rel="stylesheet"
+          crossOrigin="anonymous"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
 }
-
-export default MyDocument
+export default Document;
